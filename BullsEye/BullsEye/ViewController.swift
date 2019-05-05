fff58c1dfdeb671bc42eb23a9c513b6978da815c
Emmet <emmetlawton@gmail.com>
2019-05-05 02:46:30 +0100

    folder 3 vid 7 complete

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -12,14 +12,21 @@ import Foundation
 class ViewController: UIViewController {
     
     var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let roundedValue = slider.value.rounded();
+        //currentValue = Int(round(slider.value * 100));
+        currentValue = Int(roundedValue)
         // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
     }
     
     @IBAction func showAlert(){
-        let message = "The value of the slider is now: \(currentValue)"
+        let message = "The value of the slider is now: \(currentValue), \(targetValue)"
         
         let alert = UIAlertController(title: "Hello!", message: message, preferredStyle: .alert)
         
@@ -28,12 +35,21 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved( slider: UISlider){
         print("The value of the slider is now: \(slider.value * 100)")
-        currentValue = Int(round(slider.value * 100));
+        let roundedVaule = slider.value.rounded()
+        //currentValue = Int(round(slider.value * 100));
+        currentValue = Int(roundedVaule)
     }
-   //Complete vid 4 or folder 3
+   
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
 }
 
