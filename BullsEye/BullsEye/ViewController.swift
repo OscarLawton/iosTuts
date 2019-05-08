7b760318d92fd9e0d41e048461e8a4f093301ef3
Emmet <emmetlawton@gmail.com>
2019-05-08 02:39:55 +0100

    this should go up to github

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  BullsEye
@@ -14,9 +15,12 @@ class ViewController: UIViewController {
     var currentValue: Int = 0
     var targetValue: Int = 0
     var score = 0
+    var round = 0
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var scoreLable: UILabel!
+    @IBOutlet weak var roundLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let roundedValue = slider.value.rounded();
@@ -30,56 +34,61 @@ class ViewController: UIViewController {
         //var difference: Int
         //First soloution
         /*if currentValue > targetValue{
-            difference = currentValue - targetValue;
-        } else if (currentValue < targetValue){
-            difference = targetValue - currentValue;
-        } else {
-            difference = 0
-        }*/
+         difference = currentValue - targetValue;
+         } else if (currentValue < targetValue){
+         difference = targetValue - currentValue;
+         } else {
+         difference = 0
+         }*/
         
         //Second soloution
         /*difference = currentValue - targetValue;
-        if difference < 0{
-            difference *= -1
-        }*/
+         if difference < 0{
+         difference *= -1
+         }*/
         
         //End soloution
         let difference = abs(targetValue - currentValue);
-        let points = 100 - difference;
+        var points = 100 - difference;
         score += points
-        let message = "Your scored \(points) points!"
+        let title: String
+        if difference == 0{
+            title = "Perfect!"
+            score += 100
+        } else if difference < 2{
+            title = "Almost! Just one away!"
+            score += 50
+        } else if difference < 5 {
+            title = "Very close!"
+        } else if difference < 10 {
+            title = "Close"
+        } else {
+            title = "Not even close"
+        }
+        let message: String
+        if (round < 10){
+            message = "Your scored \(points) points!"
+        } else {
+            message = "Game over you scored \(points) points!"
+        }
+        
         /*let message = "The value of the slider is now: \(currentValue)" +
-        "\n The target value is: \(targetValue)" +
-        "\n The difference is: \(difference)"*/
+         "\n The target value is: \(targetValue)" +
+         "\n The difference is: \(difference)"*/
         
-        let alert = UIAlertController(title: "Hello!", message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: {
-            action in
-            self.startNewRound()
-        })
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-
+        if(round < 10){
+            startNewRound()
+        }
         
-        startNewRound()
-    }
-    
-    @IBAction func startOver(){
-        score = 0
-        round = 0
-        targetValue = Int.random(in: 1...100)
-        currentValue = 50
-        slider.value = Float(currentValue)
-        targetLabel.text = String(targetValue);
-        scoreLable.text = String(score);
-        roundLable.text = String(round);
-=======
         
-        startNewRound()
->>>>>>> parent of 50b3984... finished folder four
+        
     }
     
     @IBAction func sliderMoved( slider: UISlider){
@@ -88,15 +97,17 @@ class ViewController: UIViewController {
         //currentValue = Int(round(slider.value * 100));
         currentValue = Int(roundedVaule)
     }
-   
+    
     func startNewRound(){
+        round += 1
         targetValue = Int.random(in: 1...100)
         currentValue = 50
         slider.value = Float(currentValue)
         targetLabel.text = String(targetValue);
         scoreLable.text = String(score);
+        roundLable.text = String(round);
     }
-    //Completed folder 4 final vid
+    //Completed folder 4 vid 4
 }
 
 
